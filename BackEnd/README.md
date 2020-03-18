8a140655a76dda211c2ef7bdc7e71d4d16a19031
Abdelrhman Mgahed <abdelrhmanmgahed131@gmail.com>
2020-03-18 18:39:44 +0200

    edit hir

--- BackEnd/README.md
+++
@@ -1,3 +0,0 @@
-# Maimoji
-
-### Folder for BackEnd
---
+++ base.py
@@ -0,0 +1,49 @@
+import os
+from flask import *
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import *
+
+# p=rint(basdir)
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mykey'
+
+########################
+###db section###########
+########################
+basdir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basdir,'maimoji.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+####################################
+
+
+
+
+####################################
+### views functions and routing ###
+###################################
+
+@app.route('/')
+def index():
+    active = 'home'
+    return render_template('index.html',active=active)
+
+
+@app.route('/home')
+def add_pupp():
+    active = 'home'
+    pupp = puppy.query.all()
+    owners = owner.query.all()
+    return render_template('home.html',pupp=pupp,owners=owners,active=active)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__=='__main__':
+    app.run(debug=True)
