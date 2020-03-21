655e0ade40150875541c59331617b08e4661cc2b
KelzoCS <abdelrahman.27@yahoo.com>
2020-03-21 20:04:28 +0200

    rf3t sign up

---
+++ FrontEnd/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import Signup from './Signup';
+
+
+const App = () => {
+
+    return (
+
+
+        <h1>  </h1>
+
+
+
+
+    );
+
+};
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
