f9909b29a6ace9bd23b22e7b427903af2b8c4859
Abdelrhman Mgahed <abdelrhmanmgahed131@gmail.com>
2020-03-21 14:15:50 +0200

    some modifications

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 import sys
-sys.path.insert(1, 'D:/aastproject/Maimoji/dbtry')
+import os
+basdir = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(1, basdir+'/DAO')
 
 from pgdaofact import *
 
@@ -22,4 +24,3 @@ choice = 'number'
 res = aaa.getuser(number,choice)
 
 print(res)
- 
